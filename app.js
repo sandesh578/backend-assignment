a675113cc52a9aff50aacdf007d354a12de60140
sandesh578 <parajulisandesh578@gmail.com>
2023-07-05 06:25:38 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,58 @@
+const dotenv = require('dotenv');
+dotenv.config({ path: './config/.env.dev' });
+
+const path = require('path');
+const log4js = require('log4js');
+
+const DEFAULT_LOG_LEVEL = process.env.DEFAULT_LOG_LEVEL;
+const logger = log4js.getLogger(`${path.basename(__filename).split('.')[0]}`);
+const NODE_ENV = process.env.NODE_ENV;
+
+if (NODE_ENV === 'development') {
+  log4js.configure({
+    appenders: {
+      file: {
+        type: 'file',
+        filename: `logs/${path.basename(__filename).split('.')[0]}.log`
+      },
+      console: { type: 'console' }
+    },
+    categories: {
+      default: {
+        appenders: ['file', 'console'],
+        level: DEFAULT_LOG_LEVEL
+      }
+    }
+  });
+}
+
+const { app } = require('./server');
+const gracefulTermination = require('./gracefulExit');
+
+// importing routes
+const APIRouters = require('./routes/routers');
+// routers
+app.use('/', APIRouters);
+
+// global error handler
+app.use((err, req, res, next) => {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  logger.error(err);
+
+  // render the error page
+  res.status(err.status || 500);
+  const response = {
+    message: (err.data && err.data.message) || 'Internal server error',
+    status: err.status || 500
+  };
+
+  res.send(response);
+});
+
+// graceful termination of process
+process.on('SIGTERM', (error) => gracefulTermination(error, server));
+process.on('SIGINT', (error) => gracefulTermination(error, server));
+process.on('uncaughtException', (error) => gracefulTermination(error, server));
+process.on('unhandledRejection', (error) => gracefulTermination(error, server));
---
+++ server.js
@@ -0,0 +1,24 @@
+const dotenv = require('dotenv');
+const path = require('path');
+const express = require('express');
+const cors = require('cors');
+const log4js = require('log4js');
+
+const logger = log4js.getLogger(path.basename(__filename).split('.')[0]);
+dotenv.config({ path: './config/.env.dev' });
+
+const BACKEND_API_PORT = process.env.BACKEND_API_PORT;
+
+logger.info('Initializing server');
+
+const app = express();
+
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.listen(BACKEND_API_PORT, () => {
+  logger.info(`Server listening on port: ${BACKEND_API_PORT}`);
+});
+
+module.exports = { app };
